96826be8adae47fee0ae7076c27f89a79237b119
JesVre <j.vreude2000@gmail.com>
2024-01-09 14:47:14 +0100

    1st version grid visualizer of output JSON

--- Visualize_grid.py
+++ Visualize_grid.py
@@ -6,37 +6,36 @@ import json
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
+# Reads JSON file
 with open('./data/output-format.json') as f:
     data = json.load(f)
 
+# Loops over each battery
 for battery in data[1:]:
+    # Gets battery location and displays it as a green mark
     bat_loc = battery['location'].split(",")
     plt.plot(int(bat_loc[0]), int(bat_loc[1]), marker="o", markersize=3, markeredgecolor="green", markerfacecolor="green")
-
+    
+    # Loops over each house of each battery
     for house in battery['houses']:
+        # Gets house location and displays it as a red mark
         house_loc = house['location'].split(",")
         plt.plot(int(house_loc[0]), int(house_loc[1]), marker="o", markersize=3, markeredgecolor="red", markerfacecolor="red")
-        for cable in range(len(house['cables'])):
-            if cable == len(house['cables']) - 1:
-                break
-            print(cable)    
-            cable1_loc = house['cables'][cable].split(",")
-            print(cable1_loc)
-            cable2_loc = house['cables'][cable + 1].split(",")
-            plt.plot(cable1_loc, cable2_loc, 'k-', lw=1)
         
+        # Loops over each cable segment of the house 
+        for cable in range(len(house['cables'])):
+            # Skips over final point
+            if cable == len(house['cables']) - 1:
+                break 
+                
+            # Gets location of a cable point and its destination point
+            cable1_loc = house['cables'][cable].split(",")
+            cable2_loc = house['cables'][cable + 1].split(",")
+            
+            # Plots a line from the first cable point to the destination point
+            plt.plot([int(cable1_loc[0]),int(cable2_loc[0])], [int(cable1_loc[1]),int(cable2_loc[1])], 'k-', lw=1)
 
-
-"""
-for x in range(5, 10, 1):
-    plt.plot(x, 15, marker="o", markersize=3, markeredgecolor="red", markerfacecolor="red")
-    
-for x in range(0, 5, 1):
-    plt.plot(x, 10, marker="o", markersize=3, markeredgecolor="green", markerfacecolor="green")
-"""
-
-
-
+# Grid code snippet obtained from https://stackoverflow.com/questions/24943991/change-grid-interval-and-specify-tick-labels
 # Major ticks every 20, minor ticks every 5
 major_ticks = np.arange(0, 51, 10)
 minor_ticks = np.arange(0, 51, 1)
@@ -45,7 +44,6 @@ ax.set_xticks(major_ticks)
 ax.set_xticks(minor_ticks, minor=True)
 ax.set_yticks(major_ticks)
 ax.set_yticks(minor_ticks, minor=True)
-
 
 # And a corresponding grid
 ax.grid(which='both')
