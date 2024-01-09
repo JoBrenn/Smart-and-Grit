28c8e0658dc7031b3a4ecfe015a2ea46cf168b82
JesVre <j.vreude2000@gmail.com>
2024-01-09 13:07:53 +0100

    Updated visualizer

--- Visualize_grid.py
+++ Visualize_grid.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
+plt.plot(10, 10, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="red")
+plt.plot(10, 10, marker="o", markersize=5, markeredgecolor="green", markerfacecolor="green")
+
+plt.plot([10, 10], [10, 20], 'k-', lw=2)
+
 # Major ticks every 20, minor ticks every 5
 major_ticks = np.arange(0, 51, 10)
 minor_ticks = np.arange(0, 51, 1)
@@ -13,8 +18,6 @@ ax.set_xticks(minor_ticks, minor=True)
 ax.set_yticks(major_ticks)
 ax.set_yticks(minor_ticks, minor=True)
 
-plt.margins(5)
-ax.margins(5)
 
 # And a corresponding grid
 ax.grid(which='both')
@@ -22,5 +25,8 @@ ax.grid(which='both')
 # Or if you want different settings for the grids:
 ax.grid(which='minor', alpha=0.2)
 ax.grid(which='major', alpha=0.5)
+
+
+
 
 plt.show()
