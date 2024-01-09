275dc05f8affd64e42185e7d56e8fd049d1b93cc
kathymolenaar@icloud.com <kathymolenaar@icloud.com>
2024-01-09 14:45:51 +0100

    district class

--- modules/district.py
+++ modules/district.py
@@ -10,37 +10,26 @@ class District:
         self.batteries: list[Battery] = []
         self.houses: list[House] = []
         
+        # load the houses and batteries
         self.load_houses(f"../data/district_{district}/district-{district}_houses.csv")
         self.load_batteries(f"../data/district_{district}/district-{district}_batteries.csv")
     
     def load_houses(self, filename: str):
         """ loads the houses from csv file and adds them to list"""
-        number_lines = 0
+
         with open(filename) as f:
+            next(f)
             for line in f:
-                number_lines += 1
-                
-        with open(filename) as f:
-            # skip first line
-            line = f.readline()
-            for i in range(0, number_lines - 1):
-                line = f.readline()
                 house_data = line.strip().split(",")
                 house = House(house_data[0], house_data[1], house_data[2])
                 self.houses.append(house)
 
     def load_batteries(self, filename: str):
         """ loads the batteries from csv file and adds them to list"""
-        number_lines = 0
-        with open(filename) as f:
-            for line in f:
-                number_lines += 1
                 
         with open(filename) as f:
-            # skip first line
-            line = f.readline()
-            for i in range(1, number_lines):
-                line = f.readline()
+            next(f)
+            for line in f:
                 battery_data = line.strip().split(",")
                 # remove " character that comes with csv
                 battery_data[0] = battery_data[0].translate({ord('"'): None})
@@ -48,3 +37,5 @@ class District:
                 battery = Battery(battery_data[0], battery_data[1], battery_data[2], 5000)
                 self.batteries.append(battery)
 
+district = District(1)
+print(len(district.batteries))
