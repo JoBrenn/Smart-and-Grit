81739559fd261c121424467c7807f515a0533332
JesVre <j.vreude2000@gmail.com>
2024-01-09 19:52:32 +0100

    Update Visualize grid: altered grid code

--- Visualize_grid.py
+++ Visualize_grid.py
@@ -41,10 +41,7 @@ ax.set_xticks(minor_ticks, minor=True)
 ax.set_yticks(major_ticks)
 ax.set_yticks(minor_ticks, minor=True)
 
-# And a corresponding grid
-ax.grid(which='both')
-
-# Or if you want different settings for the grids:
+# Different settings for the grids:
 ax.grid(which='minor', alpha=0.2)
 ax.grid(which='major', alpha=0.5)
 
