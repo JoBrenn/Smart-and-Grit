7a58761078300210276804b62495b0401c52b97d
jobrenn <jonasbrenninkmeijer@gmail.com>
2024-01-10 11:29:52 +0100

    directory added.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
 
     district = District(int(sys.argv[1]), "costs-own")
 
+    print(district.return_output())
+
     # json_data = load_JSON_output()
     plot_output(district.return_output())
 
