64ea569fcc6de8ba08be7e4af63426383136cb93
JesVre <j.vreude2000@gmail.com>
2024-01-11 11:24:44 +0100

    Added generate cable

---
+++ code/gen_cable.py
@@ -0,0 +1,13 @@
+
+
+def get_cable_points(battery: tuple[int], house: tuple[int]) -> tuple[int]:
+    """Generates the points from house to cable"""
+
+    diff_x = battery[0] - house[0]
+    diff_y = battery[1] - house[1]
+
+    
+       
+
+
+def get_cable_segments(cable_points: tuple[int]) -> tuple[int]:
