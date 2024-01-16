88e694b68e86f9948786290c11e7425dbc1f6fd8
jobrenn <jonasbrenninkmeijer@gmail.com>
2024-01-16 12:40:22 +0100

    Cleanup

--- code/algorithms/shortest_manhattan_distance_cables.py
+++ code/algorithms/shortest_manhattan_distance_cables.py
@@ -3,18 +3,18 @@ from code.modules.battery import Battery
 
 def get_cable_points(begin: tuple[int], end: tuple[int]) -> tuple[int]:
         """ Generates the points between which a cable must be layed from house
-            to battery, following the shortest Manhatten distance 
+            to battery, following the shortest Manhatten distance
             From house first up or down then left or right"""
-           
+
         points = [begin, (begin[0], end[1]), end]
         return tuple(points)
-        
+
 def create_cable(house: House, end: tuple[int]) -> None:
     """ Creates entire cable connection between house and battery
-        following shortest manhatten distance. 
+        following shortest manhatten distance.
         Again following from house first up or donw then left or right
         post: returns the cost of the cable"""
-    
+
     cable_points = get_cable_points((house.row, house.column), (end[0], end[1]))
 
     # begin y minus in between y
@@ -22,14 +22,14 @@ def create_cable(house: House, end: tuple[int]) -> None:
     y_distance = cable_points[0][1] - cable_points[1][1]
     # In between x minus end x
     x_distance = cable_points[1][0] - cable_points[2][0]
-    
+
     # Start at the begin
     x_current = house.row
     y_current = house.column
-    
+
     # Add house coordinate
     house.add_cable_segment((x_current, y_current))
-    
+
     # Check whether we need to go up or down
     if y_distance > 0:
         # Down
@@ -43,23 +43,16 @@ def create_cable(house: House, end: tuple[int]) -> None:
         for step in range(abs(y_distance)):
             y_current += 1
             house.add_cable_segment((x_current, y_current))
-        
+
    # Check whether we need to go left or right
     if x_distance > 0:
         # Left
         for step in range(x_distance):
             x_current -= 1
             house.add_cable_segment((x_current, y_current))
-            
+
     elif x_distance < 0:
         # Right
         for step in range(abs(x_distance)):
             x_current += 1
-<<<<<<< HEAD
-            house.add_cable_segment((x_current, y_current))  
-
-    
-=======
             house.add_cable_segment((x_current, y_current))
->>>>>>> d55ee29d18832ab6948239264579b9fb305eb024
-            
