36ec141144b8d6c670d74a486ae720049401a8e7
jobrenn <jonasbrenninkmeijer@gmail.com>
2024-01-23 09:57:58 +0100

    Histogram more elaborate.

--- code/algorithms/depth_first.py
+++ code/algorithms/depth_first.py
@@ -11,7 +11,7 @@ from code.visualisation.visualize import plot_output
 def test_depth_district(district):
     depth = 5#len(district.houses)
     stack = [district]
-    
+
     #state_set = set()
     lowest_costs = float('inf')
     lowest_costs_state = None
@@ -29,18 +29,18 @@ def test_depth_district(district):
          #   .format(len(houses),state.assigned_houses,curr_depth,len(stack)))
         """if state.assigned_houses == 1:
             print("train")
-        
+
         if state.assigned_houses == 7:
             print("train2")"""
-        
-        
+
+
 
         if state.assigned_houses < depth and valid_cap(state):
             #print("hey")
-            house = houses[state.assigned_houses] 
+            house = houses[state.assigned_houses]
             #print(house)
             for n, battery in enumerate(state.batteries):
-                
+
                 child = copy.deepcopy(state)
                 house_add = copy.deepcopy(house)
                 #house cables = []
@@ -52,7 +52,7 @@ def test_depth_district(district):
                 #child.batteries[n].append(house_2)
                 stack.append(child)
 
-        else: 
+        else:
             #print(f"{state.return_json_output()}")
             total += 1
             # check solution
@@ -64,11 +64,11 @@ def test_depth_district(district):
                 lowest_costs = state.district_dict[district.costs_type]
                 lowest_costs_state = copy.deepcopy(state)
             #plot_output(state.return_output())
-            
+
     #print(f"Lowest: {lowest_costs}")
     #print(total)
     #plot_output(lowest_costs_state.return_output())
-            
+
 
 
 district = District(1, "costs-own")
@@ -83,7 +83,7 @@ def valid_cap(state):
         return True
 
 
-# Example code from Lecture Constructive algorithm by Bas Terwijn 
+# Example code from Lecture Constructive algorithm by Bas Terwijn
 def test_depth():
     depth = 10
     stack = [""]
@@ -95,10 +95,8 @@ def test_depth():
             for i in ["1","2","3","4","5"]:
                 child = copy.deepcopy(state)
                 child += i
-                #print(child)
+                # print(child)
                 stack.append(child)
-       # else: 
+       # else:
             # check solution
             #print(state)
-            
-
