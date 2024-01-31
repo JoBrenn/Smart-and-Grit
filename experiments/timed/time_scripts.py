0bbee68fe79fccf623b9aebc5e1d83236edd0d67
kathymolenaar@icloud.com <kathymolenaar@icloud.com>
2024-01-31 15:24:03 +0100

    weetnietmeer

--- experiments/timed/time_scripts.py
+++ experiments/timed/time_scripts.py
@@ -7,15 +7,25 @@ n_runs = 0
 
 if len(sys.argv) == 2 and sys.argv[1] in ["closest", "beamsearch",\
                                           "random", "hillclimber", \
-                                          "simulated", "depthfirst"]:
+                                          "simulatedannealing", "depthfirst"]:
     method = sys.argv[1]
     while time.time() - start < 2700:
         print(f"run: {n_runs}")
         subprocess.call(["timeout", "500", "python3", \
-                         "experiments/run_algs.py", f"{method}"])
+                         "experiments/timed/run_algs.py", f"{method}"])
         n_runs += 1
 
-else:
-    print("Usage: python3 time_scripts.py <algorithm>")
-    print("Algorithms: random, closest, beamsearch, hillclimber, simulated,\
-                                                                 depthfirst")
+#else:
+#    print("Usage: python3 time_scripts.py <algorithm>")
+#    print("Algorithms: random, closest, beamsearch, hillclimber, simulated,\
+#                                                                 depthfirst")
+                                                                 
+def run_time_script(method: str) -> None:
+    start = time.time()
+    n_runs = 0
+
+    while time.time() - start < 2700:
+        print(f"run: {n_runs}")
+        subprocess.call(["timeout", "500", "python3", \
+                         "experiments/timed/run_algs.py", f"{method}"])
+        n_runs += 1
