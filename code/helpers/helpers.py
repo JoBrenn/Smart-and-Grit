23be7799d7dbe93e98bf76b52d2ba5a4c6d7b29b
jobrenn <jonasbrenninkmeijer@gmail.com>
2024-01-31 13:36:35 +0100

    Cleanup

--- code/helpers/helpers.py
+++ code/helpers/helpers.py
@@ -465,7 +465,7 @@ def run_algo_method(method: str, district_number: int, runs: int) -> list:
         """
         for _ in range(runs):
             district_copy = deepcopy(district)
-            data.append(run_random_assignment_shortest_distance(district_copy, method))
+            data.append(run_random_assignment(district_copy, method))
 
     elif method == "randmanhcap":
         """
@@ -475,7 +475,7 @@ def run_algo_method(method: str, district_number: int, runs: int) -> list:
         """
         for _ in range(runs):
             district_copy = deepcopy(district)
-            data.append(run_random_assignment_shortest_distance_with_capacity(district_copy, method))
+            data.append(run_random_assignment_with_capacity(district_copy, method))
 
     elif method == "greedmanh":
         """
@@ -507,8 +507,11 @@ def run_algo_method(method: str, district_number: int, runs: int) -> list:
 
     elif method == "beamsearch":
         """
-
-
+        Beamsearch algorithm. The beam specified is the amount of states saved between
+        every iteration. An iteration is where the previous states are replaced by
+        new states that have one more connected house. In every state the same randomly
+        chosen house is connected to all batteries with enough capacity. A number
+        of best states are then kept according to the beam        
         """
         # Stop spinner, because interference with input()
         spinner.stop()
